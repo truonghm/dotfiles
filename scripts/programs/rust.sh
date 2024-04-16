0b257993dd84efcb99fd9422767521d94a09a7b3
truonghm <truong173@outlook.com>
2024-04-16 09:47:56 +0700

    update for rust, uv and spotify

---
+++ scripts/programs/rust.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "🚩🚩🚩 Installing Rust"
+
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
---
+++ scripts/programs/uv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "🚩🚩🚩 installing uv"
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
